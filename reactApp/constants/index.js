b750b1d7b6a4369105515ec772463f062d32dfc1
Ed Brown <31540673+ProgrammerDude@users.noreply.github.com>
2017-09-21 14:39:55 -0400

    Add files via upload

--- reactApp/constants/index.js
+++ reactApp/constants/index.js
@@ -1,10 +1,15 @@
 import keyMirror from '../../node_modules/fbjs/lib/keyMirror';
 
  var constants = {
-         LOAD_BUDGET: null,
-         ADD_BUDGET: null,
+        LOAD_BUDGET: null,
+        ADD_BUDGET: null,
         UPDATE_NEW_BUDGET:null,
-        UPDATE_BUDGET:null
+        UPDATE_BUDGET:null,
+        
+        EDIT_SELECTED_VALUE:null,
+        EDIT_BUDGET:null,
+        ADD_BUDGET_TO_CREATE:null
+        
 };
 
 export default keyMirror(constants);
